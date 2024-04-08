a5bb6425781dc432f74598c8b1c9c870bc61f1a0
Saif-Hossain-Mredul <atonlymesaif@gmail.com>
2024-04-08 12:15:10 +0600

    some changes and modifications on move back

--- ball-runner/Assets/Scripts/MoveObjectBack.cs
+++ ball-runner/Assets/Scripts/MoveObjectBack.cs
@@ -2,27 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoadController : MonoBehaviour
+public class MoveObjectBack : MonoBehaviour
 {
     // Start is called before the first frame update
     public float roadSpeed = 50;
-    private float length,
-        width;
 
-    void Start()
-    {
-        Vector3 size = gameObject.GetComponent<BoxCollider>().size;
-        length = size.x * 20;
-        width = size.z * 3;
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        MoveRoad();
+        MoveBack();
     }
 
-    private void MoveRoad()
+    private void MoveBack()
     {
         gameObject.transform.Translate(Vector3.back * roadSpeed * Time.deltaTime);
     }
