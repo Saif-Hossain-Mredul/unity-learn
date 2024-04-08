ce65cd67171cd316c6d15d95cee21fea80a16eab
Saif-Hossain-Mredul <atonlymesaif@gmail.com>
2024-04-08 11:51:20 +0600

    player jumping done

--- ball-runner/Assets/Scripts/PlayerController.cs
+++ ball-runner/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,26 @@ public class Player : MonoBehaviour
     public float rotationSpeed = 700;
     public float limitX = 8;
 
-    void Start() { }
+    private Rigidbody playerRb;
+    private BoxCollider playerCollider;
+    private float forceImpulseValue = 12;
+    private bool isOnRoad;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<BoxCollider>();
+        isOnRoad = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
         RotateThePlayerBall();
-
         MovePlayerHorizontally();
+        JumpPlayer();
+
+        Debug.Log(isOnRoad);
     }
 
     void RotateThePlayerBall()
@@ -31,13 +43,45 @@ public class Player : MonoBehaviour
     {
         Vector3 playerPosition = transform.position;
 
-        if (Input.GetKeyDown(KeyCode.RightArrow) && playerPosition.x < limitX)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && playerPosition.x < limitX && isOnRoad)
         {
             gameObject.transform.Translate(Vector3.right * limitX);
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && playerPosition.x > -limitX)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && playerPosition.x > -limitX && isOnRoad)
         {
             gameObject.transform.Translate(Vector3.left * limitX);
+        }
+    }
+
+    void JumpPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnRoad)
+        {
+            playerRb.AddForce(Vector3.up * forceImpulseValue, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Road")
+        {
+            isOnRoad = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Road")
+        {
+            isOnRoad = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Road")
+        {
+            isOnRoad = true;
         }
     }
 }
